a45a47ab061996a0ef15df031d503af5851c23b7
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2026-08-17 17:41:20 -0500

    added tick.js to experiment with setInterval

---
+++ scripts/tick.js
@@ -0,0 +1,15 @@
+let count = 0;
+let speed = 1000;
+
+function tick() {
+  count = count + 1;
+  console.log("Tick " + count);
+  
+	if (count === 10){
+		clearInterval(timer);
+	}
+}
+
+
+let timer = setInterval(tick, speed);
+//setInterval(tick, speed);
