761b64195c06c5ed9e1afbba220f4c68c7be9c17
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2026-08-17 17:51:31 -0500

    case-proof the style prompt

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -14,7 +14,7 @@ function randomNumber(a, b) {
 }
 
 function makeCard(){
- if (style === "fancy") {
+ if (style.toLowerCase() === "fancy") {
     console.log("Dearest " + name + ",");
   } else {
     console.log("YO " + name + "!");
